02e5b3b5f19f798f2d37034a49380747cc62a446
Manjula Dube <dube.manjula668@gmail.com>
2018-08-07 23:25:49 +0200

    style changes

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -23,8 +23,6 @@ const Section = styled.section`
   background-color: #f5f5f5;
   border: 1px solid #e3e3e3;
   border-radius: 4px;
-  -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.05);
-  box-shadow: inset 0 1px 1px rgba(0,0,0,.05);
   background-color: white;
 `
 const TextArea = styled.textarea`
