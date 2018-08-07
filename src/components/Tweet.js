ca8e8893db8434de78afb91d5c501de039537680
Manjula Dube <dube.manjula668@gmail.com>
2018-08-07 17:39:04 +0200

    add text area

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+const Section = styled.section`
+  width: 600px;
+  margin: 20px auto;
+`
+const TextArea = styled.textarea`
+  margin-top: 10px;
+  margin-left: 50px;
+  width: 500px;
+  height: 100px;
+  -moz-border-bottom-colors: none;
+  -moz-border-left-colors: none;
+  -moz-border-right-colors: none;
+  -moz-border-top-colors: none;
+  background: none repeat scroll 0 0 rgba(0, 0, 0, 0.07);
+  border-color: -moz-use-text-color #FFFFFF #FFFFFF -moz-use-text-color;
+  border-image: none;
+  border-radius: 6px 6px 6px 6px;
+  border-style: none solid solid none;
+  border-width: medium 1px 1px medium;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.12) inset;
+  color: #555555;
+  font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+  font-size: 1em;
+  line-height: 1.4em;
+  padding: 5px 8px;
+  transition: background-color 0.2s ease 0s;
+`
+
 class Tweet extends React.Component {
 	constructor(props) {
 		super(props);
@@ -60,16 +88,16 @@ addPhoto(e) {
 
 	render() {
 		return (
-			<section id="twitter">
+			<Section id="twitter">
 				{this.overflowAlert() }
         <div className="well clearfix">
-          <textarea onChange={this.handleChange.bind(this)} className="form-control"></textarea>
+          <TextArea onChange={this.handleChange.bind(this)} className="form-control"></TextArea>
           <br/>
 					<span>{this.remainingChar()}</span>
 					<button onClick={this.addPhoto.bind(this)} className="btn btn-default pull-right">{this.state.addPhotoStatus ? '✓ Photo Added' : 'Add Photo'}</button>
           <button className="btn btn-primary pull-right" disabled={this.remainingChar() === 140 || this.remainingChar() < 0}>Tweet</button>
         </div>
-       </section>
+       </Section>
 		);
 	}
 }
