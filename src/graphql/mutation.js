bbf59ecbfdd134b166419b5314cf15ac4a922f6b
Manjula Dube <dube.manjula668@gmail.com>
2018-08-12 23:14:11 +0200

    add post mutation

--- src/graphql/mutation.js
+++ src/graphql/mutation.js
@@ -16,8 +16,8 @@ export const LOGIN = gql`
  * Post tweet mutation
  */
 export const POST = gql`
-    mutation post(text: String!, upload: String){
-        post(text: text, upload: upload){
+    mutation post($text: String!, $upload: String){
+        post(text: $text, upload: $upload){
             id
             text
         }
